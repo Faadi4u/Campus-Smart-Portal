70dea3541d0bf0d50cb2eb5af051b2069e11e263
Fahad <road24u@gmail.com>
2026-01-04 12:01:08 +0500

    chore(api): scaffold backend structure with db connection and server bootstrap

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constant.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`DB CONNECTED !! DB HOST NAME: ${connectionInstance.connection.name}`);
+        
+    } catch (error) {
+        console.log("Database fails to connect!!" , error);
+        process.exit(1);
+    }
+} 
+
+export {connectDB};
